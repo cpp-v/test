f1365b1e2dd7ab789a9291f7ca6fed8a8c655504
cpp_v <cpp_v@mail.ru>
2018-11-11 21:11:36 +0300

    done

--- buttonUpp/js.js
+++ buttonUpp/js.js
@@ -1,22 +1,29 @@
+CFG_CPPV={};
+CFG_CPPV.bUpp={};
+CFG_CPPV.bUpp.uppDM="start";
 $(document).ready(function(){ 
    console.log("jQuery loaded.");  
 	$(window).scroll(function(){
 		if($(this).scrollTop()>500){
-			$("#rgArrow").attr("data-mode","ret");}
-		else{
-		   if($("#rgArrow").attr("data-mode")!="ret"){         		
-			$("#rgArrow").attr("data-mode","start");}
-			
-		}
-		});
+      	$("#rgArrow").attr("data-mode","bottom");
+   	}
+      else{
+      	$("#rgArrow").attr("data-mode",CFG_CPPV.bUpp.uppDM);
+      }		
+		
+	});
+
 	$("#rgArrow").click(function(){	
-			$("#rgArrow").attr("data-mode","ret");
-			$("body,html").animate({scrollTop:0},800);
+         if($("#rgArrow").attr("data-mode")!="bottom"){
+         	 $("body,html").animate({scrollTop:CFG_CPPV.bUpp.scrollTop},800);
+         	 return false;
+         }	
+	
+			$("body,html").animate({scrollTop:0},800);		
+			CFG_CPPV.bUpp.uppDM="ret";
 			
-			
+			CFG_CPPV.bUpp.scrollTop=$("html").scrollTop(); 
 			return false;
-			
-			
 	});
  
  
